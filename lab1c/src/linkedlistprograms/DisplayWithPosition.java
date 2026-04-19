7854af3ebf3e88c7cddbaa228896b61059dda0bc
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 18:39:10 +0530

    Update DisplayWithPosition.java

--- lab1c/src/linkedlistprograms/DisplayWithPosition.java
+++ lab1c/src/linkedlistprograms/DisplayWithPosition.java
@@ -1,4 +1,7 @@
- package linkedlistprograms;
+//1. Write a Java program to iterate through all elements in a linked list starting at the
+//specified position ( 2 nd ) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
+
+package linkedlistprograms;
 
 import java.util.*;
 
