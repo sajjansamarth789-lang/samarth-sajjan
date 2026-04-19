1408dfaf71d019cf1deefe10b117adb4546353b3
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 21:47:27 +0530

    Update Q5_RemoveWhitespace.java

--- lab3b/src/lab3/Q5_RemoveWhitespace.java
+++ lab3b/src/lab3/Q5_RemoveWhitespace.java
@@ -1,4 +1,7 @@
- package lab3;
+/*Q5. Write a Java Program for Eliminating all whitespace characters from a string using user defined
+function removeWhitespace()*/
+
+package lab3;
 
 import java.util.Scanner;
 
