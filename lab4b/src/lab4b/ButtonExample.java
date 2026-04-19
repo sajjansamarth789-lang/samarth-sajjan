b0878a1c6a03f4fd6eb2eb1d94bb8311f0f130d1
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 22:01:01 +0530

    Update ButtonExample.java

--- lab4b/src/lab4b/ButtonExample.java
+++ lab4b/src/lab4b/ButtonExample.java
@@ -1,3 +1,8 @@
+/*4b. Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is pressed”
+depending upon the Jbutton either Srilanka or India is pressed by implementing the event handling
+mechanism with addActionListener( ).*/
+
+
 package lab4b;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
