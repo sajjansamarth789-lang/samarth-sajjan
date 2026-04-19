3ae7c747ed26476272aaf24209403a21d1c4a0cf
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 18:41:02 +0530

    Update ReverseIteration.java

--- lab1c/src/linkedlistprograms/ReverseIteration.java
+++ lab1c/src/linkedlistprograms/ReverseIteration.java
@@ -1,4 +1,7 @@
- package linkedlistprograms;
+//5. Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+//Collections.swap(l_list, 0, 2))
+
+package linkedlistprograms;
 
 import java.util.*;
 
