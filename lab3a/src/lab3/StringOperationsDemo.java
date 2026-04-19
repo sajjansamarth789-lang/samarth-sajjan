636f16693cedebadecd5e7b5ddad40f2224dbf0b
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 21:10:38 +0530

    Update StringOperationsDemo.java

--- lab3a/src/lab3/StringOperationsDemo.java
+++ lab3a/src/lab3/StringOperationsDemo.java
@@ -1,4 +1,13 @@
- package lab3;
+//3a. Develop a java program for performing various string operations with different string handling
+//functions directed as follows:
+//String Creation and Basic Operations, Length and Character Access, String Comparison, String
+//Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,
+//String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and
+//startsWith() and endsWith()
+
+
+
+package lab3;
 
 import java.util.*;
 
