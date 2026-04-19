6cee8d3ba14ecdd614e6f79fdc1f17b1149875af
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 18:38:25 +0530

    Update StringPerformanceTest.java

--- lab2b/src/performance/StringPerformanceTest.java
+++ lab2b/src/performance/StringPerformanceTest.java
@@ -1,4 +1,10 @@
- package performance;
+//2.b. Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for appending
+//the string ‘AIET’ for 10000 times both in the StringBuffer and StringBuilder. Justify your answer which
+//one is better.
+
+
+
+package performance;
 
 public class StringPerformanceTest {
     public static void main(String[] args) {
