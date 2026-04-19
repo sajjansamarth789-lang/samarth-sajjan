b126a3a7395f85ab2e2d7b56366f2f05af655399
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 18:39:40 +0530

    Update InsertAtEnd.java

--- lab1c/src/linkedlistprograms/InsertAtEnd.java
+++ lab1c/src/linkedlistprograms/InsertAtEnd.java
@@ -1,4 +1,7 @@
- package linkedlistprograms;
+//3. Write a Java program to insert the specified element at the end of a linked list.( using
+//l_listobj.offerLast(&quot;Pink&quot;))
+
+package linkedlistprograms;
 
 import java.util.*;
 
